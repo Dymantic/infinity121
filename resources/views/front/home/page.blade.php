e790129e179b40c2468cf8d12698c4d94e420ddb
Michael Joyner <joyner.michael@gmail.com>
2019-07-18 08:40:52 +0800

    up and running

---
+++ resources/views/front/home/page.blade.php
@@ -0,0 +1,8 @@
+@extends('front.base')
+
+@section('content')
+    <div class="w-screen h-screen bg-black flex justify-center items-center">
+        <h1 class="text-yellow-500">Infinity</h1>
+    </div>
+@endsection
+
