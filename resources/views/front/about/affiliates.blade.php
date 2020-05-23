0504ae9e5ea892f5e2276e483b83eda7941f643b
Michael Joyner <joyner.michael@gmail.com>
2020-05-23 09:57:41 +0800

    wip

--- resources/views/front/about/affiliates.blade.php
+++ resources/views/front/about/affiliates.blade.php
@@ -3,7 +3,7 @@
         <h1 data-usher class="type-h1 mb-12 mt-20">{{ trans('about.affiliates.heading') }}</h1>
         <p class="type-b1 mt-8 max-w-2xl">{{ trans('about.affiliates.paragraph_one') }}</p>
         <div class="text-center py-20">
-            <a href="" class="type-a2">{!! trans('about.affiliates.link') !!}</a>
+            <a href="{{ localUrl("/contact-us") }}" class="type-a2">{!! trans('about.affiliates.link') !!}</a>
         </div>
     </div>
 </div>
