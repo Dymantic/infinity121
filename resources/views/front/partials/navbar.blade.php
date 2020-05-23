4c4fb46d66d241e04185b083cb17e752deb7f409
Michael Joyner <joyner.michael@gmail.com>
2020-05-23 10:06:49 +0800

    navbar responsiveness

--- resources/views/front/partials/navbar.blade.php
+++ resources/views/front/partials/navbar.blade.php
@@ -1,19 +1,19 @@
 <div
-    class="main-nav flex h-16 {{ $light ?? false ? 'text-hms-navy' : 'bg-hms-navy text-white' }} fixed w-full top-0 left-0 pl-4 md:pl-8 pr-4 md:pr-12 justify-between items-center">
+    class="main-nav flex h-16 {{ $light ?? false ? 'text-hms-navy' : 'bg-hms-navy text-white' }} fixed w-full top-0 left-0 pl-4 lg:pl-8 pr-4 lg:pr-12 justify-between items-center">
     <div>
         <a href="{{ localUrl("/") }}" class="no-underline">
             @include('svg.logos.logo_full', ['classes' => 'h-6 text-mustard hover:text-sunny-yellow'])
         </a>
     </div>
     <div
-        class="nav-drawer flex flex-col md:flex-row fixed md:static top-16 w-full md:w-auto min-h-full md:min-h-0 left-0 capitalize">
-        <a class="type-a1 tracking-wide ml-4 mt-12 mb-4 md:mb-0 md:mt-0 hover:text-mustard"
+        class="nav-drawer flex flex-col lg:flex-row fixed lg:static top-16 w-full lg:w-auto min-h-full lg:min-h-0 left-0 capitalize">
+        <a class="type-a1 tracking-wide ml-4 mt-12 mb-4 lg:mb-0 lg:mt-0 hover:text-mustard"
            href="{{ localUrl("/courses") }}">{{ trans('navbar.courses') }}</a>
-        <a class="type-a1 tracking-wide ml-4 mb-4 md:mb-0 hover:text-mustard"
+        <a class="type-a1 tracking-wide ml-4 mb-4 lg:mb-0 hover:text-mustard"
            href="{{ localUrl("/students/sign-up") }}">{{ trans('navbar.sign-up') }}</a>
-        <div class="hidden md:block group relative">
+        <div class="hidden lg:block group relative">
             <button
-                class="hidden md:inline-block type-a1 tracking-wide ml-4 mr-6 focus:outline-none dropdown-btn capitalize">{{ trans('navbar.more') }}
+                class="hidden lg:inline-block type-a1 tracking-wide ml-4 mr-6 focus:outline-none dropdown-btn capitalize">{{ trans('navbar.more') }}
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 11.92" class="w-4 ml-1 inline">
                     <path class="stroke-current" fill="none" stroke-linecap="round" stroke-linejoin="round"
                           stroke-width="4" d="M22 2L12 9.92 2 2"/>
@@ -25,12 +25,12 @@
                 </div>
             </div>
         </div>
-        <div class="md:hidden">
+        <div class="lg:hidden">
             @include('front.partials.subnav-links')
         </div>
         <a class="type-a1 tracking-wide ml-4 hover:text-mustard" href="{{ transUrl(Request::path()) }}">{{ trans('navbar.lang') }}</a>
     </div>
-    <button class="md:hidden nav-trigger hover:text-mustard">
+    <button class="lg:hidden nav-trigger hover:text-mustard">
         <svg class="w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
             <path
                 d="M16.4 9H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1zm0 4H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1zM3.6 7h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1z"/>
--- resources/views/front/partials/subnav-links.blade.php
+++ resources/views/front/partials/subnav-links.blade.php
@@ -1,7 +1,7 @@
-<a class="block my-2 pb-2 type-a1 text-white hover:text-mustard tracking-wide pl-4 border-none md:border-solid border-b md:border-black"
+<a class="block my-2 pb-2 type-a1 text-white hover:text-mustard tracking-wide pl-4 border-none lg:border-solid border-b lg:border-black"
    href="{{ localUrl("/teachers") }}">{{ trans('navbar.teachers') }}</a>
-<a class="block mb-2 pb-2 type-a1 text-white hover:text-mustard tracking-wide pl-4 border-none md:border-solid border-b md:border-black"
+<a class="block mb-2 pb-2 type-a1 text-white hover:text-mustard tracking-wide pl-4 border-none lg:border-solid border-b lg:border-black"
    href="{{ localUrl("/about-us") }}">{{ trans('navbar.about-us') }}</a>
-<a class="block mb-2 pb-2 type-a1 text-white hover:text-mustard tracking-wide pl-4 border-none md:border-solid border-b md:border-black"
+<a class="block mb-2 pb-2 type-a1 text-white hover:text-mustard tracking-wide pl-4 border-none lg:border-solid border-b lg:border-black"
    href="{{ localUrl("/contact-us") }}">{{ trans('navbar.contact') }}</a>
 <a class="block mb-2 pb-2 type-a1 text-white hover:text-mustard tracking-wide pl-4" href="{{ localUrl("/affiliates") }}">{{ trans('navbar.affiliates') }}</a>
